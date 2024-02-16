2b481e063e6a1f184daa5678dd54896092610283
Dave Guymon <davidwguymon@gmail.com>
2024-02-15 17:23:06 -0700

    Solve ETL exercise about list comprehensions

---
+++ etl/lib/etl.ex
@@ -0,0 +1,17 @@
+defmodule ETL do
+  @doc """
+  Transforms an old Scrabble score system to a new one.
+
+  ## Examples
+
+    iex> ETL.transform(%{1 => ["A", "E"], 2 => ["D", "G"]})
+    %{"a" => 1, "d" => 2, "e" => 1, "g" => 2}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    for {value, letters} <- input, letter <- letters do
+      {String.downcase(letter), value}
+    end
+    |> Map.new()
+  end
+end
