8a5e52f85b48d44645bcc2e8ba59da1cefe9a977
Dave Guymon <davidwguymon@gmail.com>
2024-03-14 14:25:50 -0600

    Solves NucleotideCount exercise.

---
+++ nucleotide-count/lib/nucleotide_count.ex
@@ -0,0 +1,38 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count(~c"AATAA", ?A)
+  4
+
+  iex> NucleotideCount.count(~c"AATAA", ?T)
+  1
+  """
+  @spec count(charlist(), char()) :: non_neg_integer()
+  def count([], _nucleotide), do: 0
+  
+  def count(strand, nucleotide) when nucleotide in @nucleotides do
+    Enum.frequencies(strand)
+    |> Map.fetch!(nucleotide)
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram(~c"AATAA")
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram(charlist()) :: map()
+  def histogram(strand) do
+    frequencies = Enum.frequencies(strand)
+    
+    Map.from_keys(@nucleotides, 0)
+    |> Map.merge(frequencies)
+  end
+end
