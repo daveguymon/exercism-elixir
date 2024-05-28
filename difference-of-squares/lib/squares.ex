0e8c07d5694361fce9a6258fcd7c6601463586fb
Dave Guymon <davidwguymon@gmail.com>
2024-05-28 11:24:28 -0600

    Solves DifferenceOfSquares exercise.

---
+++ difference-of-squares/lib/squares.ex
@@ -0,0 +1,32 @@
+defmodule Squares do
+  @moduledoc """
+  Calculate sum of squares, square of sum, difference between two sums from 1 to a given end number.
+  """
+
+  @doc """
+  Calculate sum of squares from 1 to a given end number.
+  """
+  @spec sum_of_squares(pos_integer) :: pos_integer
+  def sum_of_squares(number) do
+    Enum.to_list(1..number)
+    |> Enum.reduce(0, fn num, acc -> Integer.pow(num, 2) + acc end)
+  end
+
+  @doc """
+  Calculate square of sum from 1 to a given end number.
+  """
+  @spec square_of_sum(pos_integer) :: pos_integer
+  def square_of_sum(number) do
+    Enum.to_list(1..number)
+    |> Enum.sum
+    |> Integer.pow(2)
+  end
+
+  @doc """
+  Calculate difference between sum of squares and square of sum from 1 to a given end number.
+  """
+  @spec difference(pos_integer) :: pos_integer
+  def difference(number) do
+    square_of_sum(number) - sum_of_squares(number)
+  end
+end
